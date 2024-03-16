71341b8fcde3d030c6eb38d4e54c1e57ed3de024
Udbhas Dutta <udbhasdutta@gmail.com>
2024-03-16 17:06:45 +0530

    set up the environment for initial use, installed the necessary dependencies

---
+++ frontend/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { Slide, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
+const settings = {
+  autoClose: 3000,
+  position: "bottom-center",
+  transition: Slide
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <ToastContainer {...settings} />
+    <App />
+  </React.StrictMode>
+);
